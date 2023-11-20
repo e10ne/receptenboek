a0ff7ef6ca2d5d5fe2342ba5c52d9974136c7b95
e10ne <etiennevanhalteren@hotmail.com>
2023-11-20 15:34:16 +0100

    Update Database class with argument types

--- classes/Database.php
+++ classes/Database.php
@@ -1,14 +1,14 @@
 <?php   
     class Database extends PDO {
-        public function __construct(string $dsn, $username, $password)
+        public function __construct(string $dsn, string $username, string $password)
         {
             parent::__construct($dsn, $username, $password);
         }
 
-        public function run(string $sql, $args = null)
+        public function run(string $sql, array $arguments = [])
         {
             $statement = $this->prepare($sql);
-            $statement->execute($args);
+            $statement->execute($arguments);
             return $statement;           
         }
     }
