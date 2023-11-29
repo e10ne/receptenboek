c83d7e23d39ccf7585cee092f4f2efe1759f9efb
e10ne <etiennevanhalteren@hotmail.com>
2023-11-29 13:37:56 +0100

    Switch index and test

--- test.php
+++ test.php
@@ -1,26 +1,101 @@
 <?php
-    declare(strict_types = 1);
-    require_once("./interfaces/DatabaseOperations.php");
-    require_once("./interfaces/PageDisplay.php");
-    require_once("./classes/Database.php");
-    require_once("./classes/Recipe.php");
-    require_once("./classes/RecipeDB.php");
-    require_once("./classes/IndexPage.php");
-
-    $env = parse_ini_file(".env", false, INI_SCANNER_RAW);
-    $hostname = $env["DB_HOST"];
-    $dbname = $env["DB_NAME"];
-    $username = $env["DB_USER"];
-    $password = $env["DB_PASS"];
-    $dsn = "mysql:host=$hostname;dbname=$dbname";
-
-    $database = new Database($dsn, $username, $password);
-
-    $index = new IndexPage($database);
-
-    $limit = $_POST["limit"] ?? 3;
-
-    $index->findMultiple($limit);
-
-    echo $index->displayPage();
+   
 ?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <!-- Always force latest IE rendering engine or request Chrome Frame -->
+    <meta content="IE=edge,chrome=1" http-equiv="X-UA-Compatible">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+    <link rel="stylesheet" href="Styles/normalize.css">
+    <link rel="stylesheet" href="Styles/recipeBook.css">
+
+    <!-- Use title if it's in the page YAML frontmatter -->
+    <title>Recipe Book:</title>
+</head>
+
+<body class="gridContainer">
+
+    <header class="header flex">
+        <h1>Receptenboek:</h1>
+    </header>
+
+    <nav class="navigation">
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Add Recipe</a></li>
+            <li><a href="#">Add Category</a></li>
+        </ul>
+    </nav>
+
+    <div class="content">
+        <div class="flex flexVertical spaceAround">
+        <div></div>
+            <div class="flex flexVertical">
+                <div class="flex flexHorizontal recipeContainer">
+
+                    <a href="recipeView.php" class="recipePreviewBox flex">
+                        <div>
+                            <h3>Recipe Name</h1>
+                                <p>Recipe description</p>
+                        </div>
+                    </a>
+
+                    <a href="recipeView.php" class="recipePreviewBox flex">
+                        <div>
+                            <h3>Recipe Name</h1>
+                                <p>Recipe description</p>
+                        </div>
+                    </a>
+
+                    <a href="recipeView.php" class="recipePreviewBox flex">
+                        <div>
+                            <h3>Recipe Name</h1>
+                                <p>Recipe description</p>
+                        </div>
+                    </a>
+
+                </div>
+                <div class="flex flexHorizontal recipeContainer">
+                    <a href="recipeView.php" class="recipePreviewBox flex">
+                        <div>
+                            <h3>Recipe Name</h1>
+                                <p>Recipe description</p>
+                        </div>
+                    </a>
+
+                    <a href="recipeView.php" class="recipePreviewBox flex">
+                        <div>
+                            <h3>Recipe Name</h1>
+                                <p>Recipe description</p>
+                        </div>
+                    </a>
+
+                    <a href="recipeView.php" class="recipePreviewBox flex">
+                        <div>
+                            <h3>Recipe Name</h1>
+                                <p>Recipe description</p>
+                        </div>
+                    </a>
+                </div>
+
+            </div>
+            <div class="buttonContainer">
+                <button class="button">Show More</button>
+            </div>
+            <div></div>
+        </div>
+    </div>
+
+    <footer class="footer flex">
+        <p class="center">Gemaakt door: Etienne en Sabrina</p>
+    </footer>
+
+</body>
+
+</html>
